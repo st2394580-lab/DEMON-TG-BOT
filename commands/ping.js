dfde94f1a6515f1b56caba3586a9cfef39f990e8
st2394580-lab <st2394580@gmail.com>
2026-09-12 06:20:14 +0530

    Implement ping command with response time

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,18 @@
+module.exports = {
+  name: "ping",
 
+  async execute(ctx) {
+    const start = Date.now();
+
+    const msg = await ctx.reply("🏓 Pinging...");
+
+    const ms = Date.now() - start;
+
+    await ctx.telegram.editMessageText(
+      ctx.chat.id,
+      msg.message_id,
+      undefined,
+      `🏓 Pong!\n⚡ Response: ${ms} ms`
+    );
+  }
+};
