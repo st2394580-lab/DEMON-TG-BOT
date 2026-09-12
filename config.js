ce8ea9c6dcbc5d703581c8e247a4377a0887c479
st2394580-lab <st2394580@gmail.com>
2026-09-12 06:16:40 +0530

    Add configuration for bot settings

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
 
+module.exports = {
+  BOT_NAME: process.env.BOT_NAME || "DEMON-TG-BOT",
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  PREFIX: process.env.PREFIX || "/",
+  AUTO_REPLY: process.env.AUTO_REPLY !== "false",
+  OWNER_ID: process.env.OWNER_ID || ""
+};
