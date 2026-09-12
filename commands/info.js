468162b574fe636c2c076036babfbe7168b4fb8f
st2394580-lab <st2394580@gmail.com>
2026-09-12 06:21:23 +0530

    Add info command to reply with bot details

--- commands/info.js
+++ commands/info.js
@@ -1,1 +1,16 @@
+const config = require("../config");
 
+module.exports = {
+  name: "info",
+
+  async execute(ctx) {
+    await ctx.reply(
+      `🤖 BOT INFO\n\n` +
+      `Name: ${config.BOT_NAME}\n` +
+      `Platform: Telegram\n` +
+      `Runtime: Node.js\n` +
+      `Auto Reply: ${config.AUTO_REPLY ? "ON" : "OFF"}\n` +
+      `Prefix: ${config.PREFIX}`
+    );
+  }
+};
