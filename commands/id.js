c82fabff1436658120d5f0356558a18bdc47ccbb
st2394580-lab <st2394580@gmail.com>
2026-09-12 06:21:59 +0530

    Add command to retrieve user and chat ID information

--- commands/id.js
+++ commands/id.js
@@ -1,1 +1,16 @@
+module.exports = {
+  name: "id",
 
+  async execute(ctx) {
+    const user = ctx.from;
+    const chat = ctx.chat;
+
+    await ctx.reply(
+      `🆔 ID INFORMATION\n\n` +
+      `👤 User ID: ${user.id}\n` +
+      `💬 Chat ID: ${chat.id}\n` +
+      `👨 Name: ${user.first_name || "Unknown"}\n` +
+      `🔗 Username: ${user.username ? "@" + user.username : "None"}`
+    );
+  }
+};
