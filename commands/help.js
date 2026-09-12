639639a0d16a5719fb37d1ac9d934855ccf95e7f
st2394580-lab <st2394580@gmail.com>
2026-09-12 06:19:14 +0530

    Add help command with bot usage instructions

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,21 @@
+module.exports = {
+  name: "help",
 
+  async execute(ctx) {
+    await ctx.reply(
+      `📚 HELP\n\n` +
+      `/start - Start bot\n` +
+      `/menu - Command menu\n` +
+      `/ping - Check response\n` +
+      `/alive - Bot status\n` +
+      `/info - Bot information\n` +
+      `/id - Your Telegram ID\n` +
+      `/group - Group information\n` +
+      `/ban - Ban replied user\n` +
+      `/unban - Unban replied user\n` +
+      `/mute - Mute replied user\n` +
+      `/unmute - Unmute replied user\n\n` +
+      `💡 Reply to a user's message for moderation commands.`
+    );
+  }
+};
